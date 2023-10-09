923234715d7766bc0e990528c37d8382ce01e986
aumetros <aum3tros@yandex.ru>
2023-10-09 18:59:55 +0300

    Refactor Stories: Add module scss and mixins

--- app/components/Stories/Stories.js
+++ app/components/Stories/Stories.js
@@ -1,16 +1,16 @@
-import "./Stories.css";
 import Story from "../Story/Story";
 import { stories } from "../../utils/useData";
+import styles from "./Stories.module.scss"
 
 export default function Stories() {
   return (
-    <section id="stories" className="stories">
-      <h2 className="stories__title text-section-title">Истории путешествий</h2>
-      <p className="stories__subtitle text">
+    <section id="stories" className={styles.root}>
+      <h2 className={styles.title}>Истории путешествий</h2>
+      <p className={styles.subtitle}>
         Идейные соображения высшего порядка, а также рамки и место обучения
         кадров
       </p>
-      <div className="stories__cards">
+      <div className={styles.cards}>
         {stories.map((story, i) => {
           return <Story key={i} story={story} />;
         })}
