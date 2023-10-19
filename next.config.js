7a39d81ebea2e466b2750909c0e915756cc527d3
aumetros <aum3tros@yandex.ru>
2023-10-19 19:59:08 +0300

    Refactor NextConfig: Установлена конфигурация для разработки

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/snp-yourtour-next",
+  basePath: process.env.NODE_ENV === "production" ? "" : undefined,
   images: {
     unoptimized: true,
   },
