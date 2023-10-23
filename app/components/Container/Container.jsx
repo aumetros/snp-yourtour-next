17014411d35290879bc2481b221de002feaf1c34
aumetros <aum3tros@yandex.ru>
2023-10-23 18:18:44 +0300

    Refactor Container: Описаны дополнительные классы для Container

--- app/components/Container/Container.jsx
+++ app/components/Container/Container.jsx
@@ -3,7 +3,7 @@ import styles from "./Container.module.scss";
 export default function Container({ children, wide, mail }) {
   return (
     <div
-      className={`${styles.root} ${wide && styles.wide} ${mail && styles.mail}`}
+      className={`${styles.root} ${wide ? styles.wide : ""} ${mail ? styles.mail : ""}`}
     >
       {children}
     </div>
